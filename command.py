e498ad0007851694d95fdc0b3fa51ad1b9027da7
Riqk <rick.theeuwes@hotmail.nl>
2019-09-18 13:37:03 +0200

    redone file structure

--- command.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from config import config
-
-#runs shell command
-def run(command, capture_output=False, check=True):
-    #if capture_output is True, text doens't work, sadly...
-    return subprocess.run(command, capture_output=capture_output, check=check, text=config.getStatic('enableOutput'))
--- exit.py
+++
@@ -1,6 +0,0 @@
-from chache import chache
-from config import config
-
-def quit(): #runs on exit, for doing cleanup
-    chache.clear()
-    config.writeStatic()
