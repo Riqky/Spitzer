3de05089e87c70feeb1030391c91f3bae2233373
Riqk <rick.theeuwes@hotmail.nl>
2019-11-01 11:48:17 +0100

    test

--- setup.sh
+++ setup.sh
@@ -7,29 +7,45 @@
 
 
 #TODO maybe first check for pip and python version
-pip install . > /dev/null
+'''npip install . 
+
+
+mkdir /tmp/spitzer
 
 #aquatone
-mkdir /tmp/spitzer > /dev/null
-
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -P /tmp/spitzer/ > /dev/null
-unzip /tmp/spitzer/aquatone_linux_amd64_1.7.0.zip -d /tmp/spitzer > /dev/null
-mv /tmp/spitzer/aquatone /usr/local/bin/ > /dev/null
+if [-z which aquatone]
+then
+    wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -P /tmp/spitzer/ 
+    unzip /tmp/spitzer/aquatone_linux_amd64_1.7.0.zip -d /tmp/spitzer 
+    mv /tmp/spitzer/aquatone /usr/local/bin/ 
+fi
 
 #gobuster
-wget https://github.com/OJ/gobuster/releases/download/v3.0.1/gobuster-linux-amd64.7z -P /tmp/spitzer/
-7z x /tmp/spitzer/gobuster-linux-amd64.7z -o/tmp/spitzer/
-mv /tmp/spitzer/gobuster-linux-amd64/gobuster /usr/local/bin/
-chmod +x /usr/local/bin/gobuster
-
-rm -r /tmp/spitzer
+if [-z which gobuster]
+then
+    wget https://github.com/OJ/gobuster/releases/download/v3.0.1/gobuster-linux-amd64.7z -P /tmp/spitzer/
+    7z x /tmp/spitzer/gobuster-linux-amd64.7z -o/tmp/spitzer/
+    mv /tmp/spitzer/gobuster-linux-amd64/gobuster /usr/local/bin/
+    chmod +x /usr/local/bin/gobuster
+fi
 
 #securityheaders.py
-wget https://raw.githubusercontent.com/juerkkil/securityheaders/master/securityheaders.py -P /opt/spitzer/ > /dev/null
+wget https://raw.githubusercontent.com/juerkkil/securityheaders/master/securityheaders.py -P /opt/spitzer/ 
 
 #testssl.sh
-wget https://raw.githubusercontent.com/drwetter/testssl.sh/3.0/testssl.sh -P /opt/spitzer/ > /dev/null
+wget https://raw.githubusercontent.com/drwetter/testssl.sh/3.0/testssl.sh -P /opt/spitzer/''' 
 
-#TODO maybe reinstall masscan, to make sure it is the latest version
-#or ask this
+#masscan
+mass=`masscan --version`
+massversion=${mass:16:6}
+massnum=`echo "$massversion" | tr -d .`
+if[massnum -lt 106]
+then
+#TODO clear current masscan
+#TODO ask?
+    git clone https://github.com/robertdavidgraham/masscan /tmp/spitzer
+    make /tmp/spitzer/masscan
+    mv /tmp/spitzer/masscan/bin/masscan /usr/local/bin/
+fi
 
+#rm -r /tmp/spitzer
