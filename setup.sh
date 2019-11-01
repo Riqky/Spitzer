d37892f4b75c384c2ff01d7b8085e996b816c763
Riqk <rick.theeuwes@hotmail.nl>
2019-11-01 11:55:23 +0100

    ugh...

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 
 #TODO maybe first check for pip and python version
-: 'npip install . 
+: 'pip install . 
 
 
 mkdir /tmp/spitzer
@@ -39,7 +39,7 @@ wget https://raw.githubusercontent.com/drwetter/testssl.sh/3.0/testssl.sh -P /op
 mass=`masscan --version`
 massversion=${mass:16:6}
 massnum=`echo "$massversion" | tr -d .`
-if[massnum -lt 106]
+if[$massnum -lt 106]
 then
 #TODO clear current masscan
 #TODO ask?
